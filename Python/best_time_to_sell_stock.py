90ff0c86ed5916e51285bc5daf58ce1e4454550f
mohammedsaif <mohammedsaifrockstar@gmail.com>
2021-06-10 02:42:23 +0100

    Update best_time_to_sell_stock.py

--- Python/best_time_to_sell_stock.py
+++ Python/best_time_to_sell_stock.py
@@ -2,4 +2,7 @@
 
 def max_profit_from_stock(prices[]):
   max_profit = 0
+  for i in range(1,len(prices)):
+    if prices[i] > prices[i-1]:
+      max_profit += prices[i]-prices[i-1]
   return max_profit
